5647dbf3e49edb85cf656ff8e2dcbb4ac5610cb7
mortalcr <daporrasn@gmail.com>
2024-11-26 11:06:11 -0600

    feat: navBar and manage session

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <NavBar />
     <NuxtPage />
     <UNotifications />
   </div>
---
+++ components/navBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav v-if="!esLogin" class="flex justify-between items-center p-4 text-white">
+    <h1 class="text-xl font-bold">Mi Aplicación</h1>
+    <button 
+      class="text-black font-bold py-2 px-4 rounded" 
+      @click="cerrarSesion"
+    >
+      Cerrar Sesión
+    </button>
+  </nav>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const esLogin = computed(() => route.path === '/login')
+
+function cerrarSesion() {
+  localStorage.removeItem('token'); 
+  window.location.reload();
+}
+</script>
+
+<style scoped>
+nav {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+</style>
