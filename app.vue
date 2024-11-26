84469e303b5b5e11b5aeda9e75ab5226f5766a41
noni <noni45100@hotmail.com>
2024-11-26 11:41:58 -0600

    feat: add login check on component mount

--- app.vue
+++ app.vue
@@ -7,5 +7,23 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'; // Para ejecutar código cuando el componente ya está montado
+import { useRouter } from 'vue-router'; // Importar el router
 
+const router = useRouter();
+
+// Verifica si el usuario está logueado
+function ifLogin() {
+  const token = localStorage.getItem('token');
+  if (token === null) {
+    router.push('/login');
+  } else {
+    console.log(token, "hola");
+  }
+}
+
+// Ejecuta la verificación solo en el cliente después de montar el componente
+onMounted(() => {
+  ifLogin();
+});
 </script>
