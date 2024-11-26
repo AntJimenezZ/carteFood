da2d37d0c81f3589bfa9e3473fa9999a2b1615f5
noni <noni45100@hotmail.com>
2024-11-26 11:36:33 -0600

    refactor: remove ifLogin function and router logic from app.vue

--- app.vue
+++ app.vue
@@ -7,18 +7,5 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
 
-const router = useRouter();
-
-async function ifLogin() {
-  const token = localStorage.getItem('token');
-  if (token === null) {
-    router.push('/login');
-  } else {
-    console.log(token, "hola");
-  }
-}
-
-ifLogin();
 </script>
