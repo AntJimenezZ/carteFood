a0e5ca1792f7b7c02011c8148f2749bcc61dc6d0
mortalcr <daporrasn@gmail.com>
2024-11-26 10:55:50 -0600

    feat: ifLogin

--- app.vue
+++ app.vue
@@ -4,3 +4,20 @@
     <UNotifications />
   </div>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+async function ifLogin() {
+  const token = localStorage.getItem('token');
+  if (token === null) {
+    router.push('/login');
+  } else {
+    console.log(token, "hola");
+  }
+}
+
+ifLogin();
+</script>
